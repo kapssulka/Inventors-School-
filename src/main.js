1c3f3e74fa9c9238916ca5f095801e6670b0d716
Danil Tsybulin <https://git.staffjet.ru/>
2026-08-25 11:16:20 +0600

    Add vars: colors and font

--- src/main.js
+++ src/main.js
@@ -1,2 +1,8 @@
+// base
 import "./scss/index.scss";
 import "modern-css-reset";
+
+// fonts
+import "@fontsource-variable/manrope/wght.css";
+import "@fontsource/lora/400.css";
+import "@fontsource/lora/500.css";

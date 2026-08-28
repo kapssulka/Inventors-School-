d37e00fa203b789d8e8ab6d1d121bd9a69131a73
Danil Tsybulin <https://git.staffjet.ru/>
2026-08-28 18:29:46 +0600

    Edits id for metrix

--- src/js/metrix.js
+++ src/js/metrix.js
@@ -43,7 +43,7 @@ const signupButtons = document.querySelectorAll('[data-metric="cta_signup"]');
 if (signupButtons.length > 0) {
   signupButtons.forEach((button) => {
     button.addEventListener("click", () => {
-      ym(COUNTER_ID_signup, "reachGoal", "signup_click", {
+      ym(COUNTER_ID_signup, "reachGoal", "cta_signup", {
         section: button.dataset.section,
       });
 
@@ -58,7 +58,7 @@ const programButtons = document.querySelectorAll('[data-metric="cta_program"]');
 if (programButtons.length > 0) {
   programButtons.forEach((button) => {
     button.addEventListener("click", () => {
-      ym(COUNTER_ID_pogram, "reachGoal", "program_click", {
+      ym(COUNTER_ID_pogram, "reachGoal", "cta_program", {
         section: button.dataset.section,
         program: button.dataset.program,
       });
